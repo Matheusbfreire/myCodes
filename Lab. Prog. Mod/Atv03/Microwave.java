2255a7db79d5b7bc8622c2f1ddfcadf01e2b56ba
Matheus Freire <matheusbrandaof1@gmail.com>
2022-03-11 08:40:26 -0300

    Updating files

--- Lab. Prog. Mod/Atv03/Microwave.java
+++ Lab. Prog. Mod/Atv03/Microwave.java
@@ -1,24 +1,89 @@
-class Microwave {
-    int minutes;
-    int seconds;
+package microwavesrc;
 
-    public int getMinutes() {
-        return this.minutes;
-    }
+public class Microwave {
+	int minutes;
+	int seconds;
+	boolean paused = false;
+	boolean on = false;
+	boolean doorOpen = false;
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
+	public int getMinutes() {
+		return minutes;
+	}
 
-    public int getSeconds() {
-        return this.seconds;
-    }
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
 
-    public void setSeconds(int seconds) {
-        this.seconds = seconds;
-    }
-    
-    public void timer(int minutes, int seconds) {
-        
-    }
+	public int getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
+	}
+
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean on) {
+		this.on = on;
+	}
+
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+
+	public void setDoorOpen(boolean doorOpen) {
+		this.doorOpen = doorOpen;
+	}
+	/* Esse método inicia o timer do microondas de acordo com os minutos
+	 * e segundos passados por parâmetro. */
+	public void timer(int minutes, int seconds) {
+		setMinutes(minutes);
+		setSeconds(seconds);
+	}
+	/* Esse método pausa o timer do microondas. */
+	public void pause() {
+		setPaused(true);
+	}
+	/* Esse método reinicia o timer do microondas. */
+	public void reeset() {
+		setSeconds(0);
+		setMinutes(0);
+	}
+	/* Esse método desliga o microondas. */
+	public void turnOff() {
+		setOn(false);
+	}
+	/* Esse método liga o microondas somente se a porta estiver fechada. */
+	public void turnOn() {
+		if (!this.doorOpen) {
+			setOn(true);
+		} else {
+			new Exception("ERRO: Porta aberta, impossível ligar o microondas");
+		}
+	}
+	/* Esse método abre a porta do microondas somente se ele estiver desligado. */
+	public void openTheDoor() {
+		if (!this.on) {
+			setDoorOpen(true);
+		} else {
+			new Exception("ERRO: Microondas ligado, impossível abrir a porta");
+		}
+	}
+	/* Esse método fecha a porta do microondas. */
+	public void closeTheDoor() {
+		setDoorOpen(false);
+	}
+	
 }
--- Lab. Prog. Mod/Atv03/MicrowaveTest.java
+++ Lab. Prog. Mod/Atv03/MicrowaveTest.java
@@ -1,17 +1,66 @@
+package microwavesrc;
+
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupter.api.Assertions.*;
 
-public class MicrowaveTest {
+class MicrowaveTest {
 
-    @Test
-    public void timer() {
-        Microwave m = new Microwave();
+	@Test
+	void timerTest() {
+		Microwave m = new Microwave();
+		
+		m.timer(10, 10);
+		
+		assertEquals(10, m.minutes);
+		assertEquals(10, m.seconds);
+	}
+	
+	@Test
+	void pausingTimerTest() {
+		Microwave m = new Microwave();
+		
+		m.pause();
+		
+		assertTrue(m.paused);
+	}
+	
+	@Test
+	void reesetingTimerTest() {
+		Microwave m = new Microwave();
+		
+		m.reeset();
+		
+		assertEquals(0, m.minutes);
+		assertEquals(0, m.seconds);
+	}
+	
+	@Test
+	void turningTheMicrowaveOffTest() {
+		Microwave m = new Microwave();
+		
+		m.turnOff();
+		
+		assertFalse(m.on);
+	}
+	
+	@Test
+	void turningTheMicrowaveOnIfTheDoorIsClosedTest() {
+		Microwave m = new Microwave();
 
-        m.timer(10, 10);
-        int minutes = m.getMinutes();
-        int seconds = m.getSeconds();
+		m.turnOn();
 
-        assertEquals(10, minutes);
-        assertEquals(10, seconds);
-    }
+		assertFalse(m.doorOpen);
+		assertTrue(m.on);
+	}
+	
+	@Test
+	void openingTheDoorIfTheMicrowaveIsOff() {
+		Microwave m = new Microwave();
+
+		m.openTheDoor();
+
+		assertFalse(m.on);
+		assertTrue(m.doorOpen);
+	}
+	
 }
